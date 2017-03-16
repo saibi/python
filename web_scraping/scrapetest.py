9c5a69b8e19a270bb8c659147a5043d30202a5e4
Youngmin Kim <saibi@saibi-macbookpro.local>
2017-03-16 22:36:29 +0900

    web scraping

---
+++ web_scraping/scrapetest.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+
+from urllib.request import urlopen
+
+url = input("Enter URL : ")
+if url == "" :
+	url = "http://pythonscraping.com/pages/page1.html"
+
+if "http://" not in url :
+	url = "http://" + url
+
+html = urlopen(url)
+print(html.read())
