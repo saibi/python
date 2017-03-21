083063e5ee65172c86cc3735c1b4acfed6cd8818
Youngmin Kim <ymkim@huvitz.com>
2017-03-22 08:37:34 +0900

    bs child, parent

---
+++ web_scraping/child01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj = BeautifulSoup(html)
+
+for child in bsObj.find("table", {"id":"giftList"}).children:
+	print(child)
+
---
+++ web_scraping/parent01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj = BeautifulSoup(html)
+
+sibling = bsObj.find("img", {"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text()
+print(sibling)
+
