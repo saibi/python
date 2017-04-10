53480b52a051488eb031891c5c68002a18c2eaea
Youngmin Kim <saibi@saibi-macbookpro.local>
2017-04-10 22:14:02 +0900

    list

---
+++ tutorial/list01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+squares = []
+for x in range(10):
+	squares.append(x**2)
+
+print(squares)
+
+squares = list(map(lambda x: x**2, range(10)))
+print(squares)
+
+
+squares = [x**2 for x in range(10)]
+print(squares)
