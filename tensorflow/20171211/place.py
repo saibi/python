51d76e1bb68bd57a56cfaf479f10d1ab55726b82
Youngmin Kim <kimyoungmin@gmail.com>
2017-12-11 09:26:21 +0900

    linear regression

---
+++ tensorflow/20171211/place.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+
+
+
+#
+# H(x) = Wx + b
+#
+W = tf.Variable(tf.random_normal([1]), name='weight') # [1] <- shape , 1Dimension
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# X and Y data
+X = tf.placeholder(tf.float32, shape=[None])
+Y = tf.placeholder(tf.float32, shape=[None])
+
+# Our hypothesis WX + b
+hypothesis = X * W + b
+
+
+
+
+#
+# cost(W,b) = 1/m  *  sigma i=1 to m ( H(x(i)) - y(i) )^2
+#
+
+# cost/loss function
+cost = tf.reduce_mean( tf.square(hypothesis - Y))
+
+# minimize
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01)
+train = optimizer.minimize(cost)
+
+
+
+
+
+
+# launch the graph in a session
+
+sess = tf.Session()
+
+# initialize global variables in the graph
+
+sess.run(tf.global_variables_initializer())
+
+# fit the line
+
+for step in range(2001):
+	cost_val, W_val, b_val, _ = sess.run( [cost, W, b, train], 
+			feed_dict = { X: [1, 2, 3, 4, 5], 
+				Y: [1, 2, 3, 4, 5] } )
+	
+	if step % 20 == 0 :
+		print(step, cost_val, W_val, b_val)
+
+
+# testing our model
+print( sess.run(hypothesis, feed_dict={X:[5]}))
+print( sess.run(hypothesis, feed_dict={X:[6]}))
+print( sess.run(hypothesis, feed_dict={X:[2.5]}))
+print( sess.run(hypothesis, feed_dict={X:[1.5, 3.5]}))
+
