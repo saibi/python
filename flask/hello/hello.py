bd8e70f6a0c1f5d2aee44346ce46f1aba7fcda16
Youngmin Kim <kimyoungmin@gmail.com>
2022-06-11 17:32:02 +0900

    flask: route

--- flask/hello/hello.py
+++ flask/hello/hello.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+@app.route('/home')
+def home():
     return 'Hello, World!!!'
+
+@app.route('/user')
+def user():
+    return 'Hello, User!!!'
 
 if __name__ == "__main__":
     app.run()
