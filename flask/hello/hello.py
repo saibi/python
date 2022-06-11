c2bd5d4d087eab0bf3521c2b499d796b77c60f88
Youngmin Kim <kimyoungmin@gmail.com>
2022-06-11 22:17:25 +0900

    flask:return html

--- flask/hello/hello.py
+++ flask/hello/hello.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hello, World!!!'
+    return '''
+    <h1>Hello, World!!!</h1>
+    <p>html contents</p>
+    <a href="http://127.0.0.1:5000/user/saibi/18">dynamic url</a>
+    '''
 
 @app.route('/user/<user_name>/<int:user_id>')
 def user(user_name, user_id):
