f72122413077a6deb9df02c7fa74a1caa7253059
Youngmin Kim <kimyoungmin@gmail.com>
2022-06-17 14:59:04 +0900

    flask: uri var

--- flask-restx/hello/app.py
+++ flask-restx/hello/app.py
@@ -4,10 +4,10 @@ from flask_restx import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-@api.route('/hello')
+@api.route('/hello/<string:name>')
 class HelloWorld(Resource):
-    def get(self):
-        return {"hello":"world"}
+    def get(self, name):
+        return {"message":"Welcome, %s!" % name}
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
