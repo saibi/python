cf54073057ccb9f00e9722ba56e006a4821c02b0
Youngmin Kim <kimyoungmin@gmail.com>
2022-06-20 13:32:25 +0900

    flask: delete self-signed cert

--- flask/https_hello/app.py
+++ flask/https_hello/app.py
@@ -1,4 +1,8 @@
 # $ conda install pyopenssl
+#
+# create self-signed cert 
+# $ openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
+#
 from flask import Flask
 app = Flask(__name__)
 
