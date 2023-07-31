d720b6da6827b0fdfff0d74170fa0bd6f90c569c
Youngmin Kim <kimyoungmin@gmail.com>
2023-07-31 12:45:21 +0900

    angela 20: snake direction method

--- angela_course/section20_snake_game/snake.py
+++ angela_course/section20_snake_game/snake.py
@@ -3,12 +3,18 @@ from turtle import Turtle
 SEGMENT_SIZE = 20
 STARTING_POSITIONS = [(0, 0), (-SEGMENT_SIZE, 0), (-SEGMENT_SIZE*2, 0)]
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
 
     def __init__(self) -> None:
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for pos in STARTING_POSITIONS:
@@ -23,4 +29,20 @@ class Snake:
             pos = self.segments[seg_num-1].position()
             self.segments[seg_num].goto(pos)
 
-        self.segments[0].forward(SEGMENT_SIZE)
+        self.head.forward(SEGMENT_SIZE)
+
+    def up(self) -> None:
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self) -> None:
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self) -> None:
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self) -> None:
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
