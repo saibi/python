91639ff20e948839229b76adcac09867a8e39fa5
Youngmin Kim <kimyoungmin@gmail.com>
2023-08-23 10:06:53 +0900

    angela 23: turtle crossing

---
+++ angela_course/section23_turtle_crossing/car_manager.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+CAR_START_X = 300
+CAR_MAX_Y = 250
+
+
+class CarManager():
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        random_change = random.randint(1, 6)
+        if random_change == 1:
+            new_car = Turtle("square")
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.goto(CAR_START_X, random.randint(-CAR_MAX_Y, CAR_MAX_Y))
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
